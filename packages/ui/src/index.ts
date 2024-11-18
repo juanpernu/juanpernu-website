6d9cbc3abc3732a8b667192afe1d51bde0a3f79b
Juan Manuel Pernumian <juan.pernumian@gmail.com>
2024-11-18 19:26:24 -0300

    feat(lerna): building monorepo with lerna

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,11 @@
+export { Button, buttonVariants } from "./components/ui/button";
+export { Dock, DockIcon } from "./components/ui/dock";
+export { DotPattern } from "./components/ui/dot-pattern";
+export { Separator } from "./components/ui/separator";
+export {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./components/ui/tooltip";
+export { Toggle, toggleVariants } from "./components/ui/toggle";
